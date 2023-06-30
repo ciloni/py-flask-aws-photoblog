f07df47d100429a593d3b905420461c9c6435da3
ciloni <45293934+ciloni@users.noreply.github.com>
2023-06-30 17:37:28 -0300

    Aula 4 parte 1
    Criei o formulário de login e método de login nao app controler para verificar se a senha corresponde, rota de login

--- src/controllers/AppController.py
+++ src/controllers/AppController.py
@@ -1,4 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from src.models.UsersModel import User, db
+from werkzeug.security import check_password_hash
 
 def index():
     return render_template('index.html')
+
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if User.query.filter_by(user_email=email).count() > 0:
+            user = db.one_or_404(db.select(User).filter_by(user_email = email))
+            if check_password_hash(user.user_password, password):
+                return "senha correta"
+            else:
+                return "senha incorreta"
+            # redirect(url_for('app_routes.login'))
+        # TO DO SEGURANCA DE LOGIN
+        return "Ainda não é possível fazer login"
+    return render_template('login.html')
--- src/routes/AppRoutes.py
+++ src/routes/AppRoutes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
-from src.controllers.AppController import index
+from src.controllers.AppController import index, login
 
 AppRoutes = Blueprint('app_routes',__name__)
 
 AppRoutes.route('/', methods=['GET'])(index)
+AppRoutes.route('/login',methods=['GET','POST'])(login)
